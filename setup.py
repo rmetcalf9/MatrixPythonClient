cb8febad203b9517ffe2d033fd6cb49cf9323d19
Robert Metcalf <rmetcalf9@googlemail.com>
2025-11-18 13:23:13 +0000

    Update dependancies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import versioneer
 sp_install_requires = [
   'requests==2.31.0',
   'pytz==2025.2',
-  'python-dateutil==2.8.1',
+  'python-dateutil==2.9.0.post0',
   'PythonAPIClientBase==0.0.16',
   'InquirerPy==0.3.4'
 ]
